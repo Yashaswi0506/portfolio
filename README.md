70e02b7cc96806aa1bd3d1b04c4c78d54aadbd49
Tushar Jain <yashaswi@pdx.edu>
2023-03-08 02:38:41 -0800

    hamburger implementation done

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # webdevelpment_project
 Web Development Project 
+
+---------- issues------------------------------------
 1. flex overfixed block
 2. cards of different heights
 3. streched image : object cover 
 4. zindex : 9999
+5. downloadable font: rejected by sanitizer (font-family: "unicons-line" style:normal weight:400 stretch:100 src index:1) source: https://unicons.iconscout.com/release/v4.0.0/fonts/line/unicons-16.woff2 : linkedin
 
 
 -----------fix--------------------
 1. ul for accordian
+2. check scroll top : -50 js or remove
 
 
 
 ----------To DO---------
 1. form size
-2. image
+2. image size
 3. font
-4. hamburger
-5. icons
-6. 
+4. icons
+6. bootstrap
+
+
+
+---done---
+hamburger
--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const username = document.querySelector('#username');
 const email = document.querySelector('#email');
 const message = document.querySelector('#message');
 const submit = document.querySelector('#submit');
+const hamburger = document.querySelector(".hamburger");
+const navsub = document.querySelector(".ul_class");
+
+hamburger.addEventListener('click', () => {
+    console.log("changing");
+    navsub.classList.toggle("active");
+});
+
+document.querySelectorAll(".box").forEach(n => n.addEventListener("click", () => {
+    navsub.classList.remove("active");
+  }))
+
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    navsub.style.top = '0';
+    
+    
+  } else {
+    navsub.style.top = '-50px';
+  }
+} 
 
 
 submit.addEventListener('click', handleClick);
@@ -11,7 +38,7 @@ function handleClick(event){
     console.log("Hello");
     
     if(username.value === '' || username.value === null){
-        alert('Name is required');
+        alert('Name required');
     }
 
      if(email.value === '' || email.value === null){
@@ -23,4 +50,6 @@ function handleClick(event){
         alert('Message should contain atlease 30 words');
     }
 
+
 }
+
