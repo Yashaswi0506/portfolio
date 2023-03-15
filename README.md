4df5348396759924a3c0045567226d03591039f1
Tushar Jain <yashaswi@pdx.edu>
2023-03-15 14:44:42 -0700

    Final Commit1

--- README.md
+++ README.md
@@ -3,10 +3,14 @@ This site is developed in HTML , CSS , Javascript and Bootstrap
 It is deployed at : https://yashaswi0506.github.io/webdevelpment_project/
 
 To run this project click on index.html or open index.html using browser or click deployment link
+
 Image folder contians images used in this project
 
 List of Resources used:
 Transition hover over particular element : https://youtu.be/RvkHNGFRwEc.
+
 Form Validation : https://youtu.be/fz8bwvn9lA4
+
 Hamburger Implementation:  https://www.youtube.com/watch?v=flItyHiDm7E
+
 Image zoom out issue : https://stackoverflow.com/questions/11757537/css-image-size-how-to-fill-but-not-stretch
--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const username = document.querySelector("#username");
-const email = document.querySelector("#email");
-const message = document.querySelector("#message");
-const submit = document.querySelector("#submit");
+const form = document.querySelector("form");
+const username = form.querySelector("#username");
+const email = form.querySelector("#email");
+const message = form.querySelector("#message");
+const submit = form.querySelector("#submit");
 const hamburger = document.querySelector(".hamburger");
 const navsub = document.querySelector(".ul_class");
-var error = document.querySelector("#error");
-const reset = document.querySelector("#reset");
+let error = form.querySelector("#error");
+const reset = form.querySelector("#reset");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", smallscreen);
+
+function smallscreen(event) {
   navsub.classList.toggle("active");
-});
-
-document.querySelectorAll(".box").forEach((n) =>
-  n.addEventListener("click", () => {
-    navsub.classList.remove("active");
-  })
-);
+  document.querySelectorAll(".box").forEach((list_item) =>
+    list_item.addEventListener("click", () => {
+      navsub.classList.remove("active");
+    })
+  );
+}
 
 submit.addEventListener("click", handleClick);
 
 function handleClick(event) {
-  console.log("Hello");
   error.textContent = "";
-
 
   if (username.value === "" || username.value === null) {
     event.preventDefault();
@@ -41,14 +41,18 @@ function handleClick(event) {
     message.value === "" ||
     message.value === null
   ) {
-    
+    event.preventDefault();
     error.innerHTML += "Enter atleast 5 words<br>";
     error.style.color = "red";
   }
-  
+
+  if (error.textContent === "") {
+    error.innerHTML = "Form Submitted Successfully";
+    error.style.color = "green";
+    event.preventDefault();
+  }
 }
 reset.addEventListener("click", handleClick1);
 function handleClick1(event) {
-  console.log("Hello");
   error.textContent = "";
 }
